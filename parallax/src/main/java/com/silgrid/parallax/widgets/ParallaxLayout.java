ccd049c24c8ca3bc38b431ba6f1fca36717efc64
Andrii Senkiv <ezfero@gmail.com>
2016-10-11 22:03:45 +0300

    Added vertical scrolling for background. Changed images.

--- parallax/src/main/java/com/silgrid/parallax/widgets/ParallaxLayout.java
+++ parallax/src/main/java/com/silgrid/parallax/widgets/ParallaxLayout.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.IntDef;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -13,8 +14,20 @@ import android.widget.FrameLayout;
 import com.silgrid.parallax.R;
 import com.silgrid.parallax.sensor.TiltSensor;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 public class ParallaxLayout extends FrameLayout implements TiltSensor.SensorCallback {
 
+	@Retention(RetentionPolicy.SOURCE)
+	@IntDef({DIRECTION_HORIZONTAL, DIRECTION_VERTICAL, DIRECTION_BOTH})
+	public @interface ParallaxDirection {}
+
+	public static final int DIRECTION_HORIZONTAL = 0x01;
+	public static final int DIRECTION_VERTICAL = 0x10;
+	public static final int DIRECTION_BOTH = 0x11;
+
+	private int mDirection = DIRECTION_HORIZONTAL;
 	private int mParallaxSpeed = 3;
 	private float mTranslationX;
 	private float mTranslationY;
@@ -37,11 +50,12 @@ public class ParallaxLayout extends FrameLayout implements TiltSensor.SensorCall
 
 		TypedArray a = context.getTheme().obtainStyledAttributes(
 				attrs,
-				R.styleable.ParallaxImageView,
+				R.styleable.ParallaxLayout,
 				0, 0);
 
 		try {
 			mParallaxSpeed = a.getInt(R.styleable.ParallaxLayout_parallaxBackgroundSpeed, mParallaxSpeed);
+			mDirection = a.getInt(R.styleable.ParallaxLayout_parallaxBackgroundDirection, mDirection);
 		} finally {
 			a.recycle();
 		}
@@ -111,7 +125,14 @@ public class ParallaxLayout extends FrameLayout implements TiltSensor.SensorCall
 
 	@Override
 	public void onRotationChanged(float xAngle, float yAngle) {
-		mTranslationX = angleToTranslation(xAngle);
+		if ((mDirection & DIRECTION_HORIZONTAL) != 0) {
+			mTranslationX = angleToTranslation(xAngle, DIRECTION_HORIZONTAL);
+		}
+		if ((mDirection & DIRECTION_VERTICAL) != 0) {
+			Log.d("debug", "angle " + yAngle);
+			mTranslationY = angleToTranslation(yAngle, DIRECTION_VERTICAL);
+			Log.d("debug", "mTranslationY " + mTranslationY);
+		}
 		invalidate();
 
 		for (int i = 0; i < getChildCount(); ++i) {
@@ -133,15 +154,18 @@ public class ParallaxLayout extends FrameLayout implements TiltSensor.SensorCall
 	}
 
 	private void updateBackgroundBounds(int width, int height) {
-		boolean isLandscapeImage = mBackground.getIntrinsicWidth() > mBackground.getIntrinsicHeight();
-
 		int bgWidth = width;
 		int bgHeight = height;
-		if (isLandscapeImage) {
-			float ratio = mBackground.getIntrinsicWidth() > width
+
+		if (mBackground.getIntrinsicWidth() > mBackground.getIntrinsicHeight()) {
+			boolean cropWidth = mBackground.getIntrinsicWidth() > width;
+			float ratio = cropWidth
 					? (float) height / mBackground.getIntrinsicHeight()
 					: (float) width / mBackground.getIntrinsicWidth();
 			bgWidth = (int) (mBackground.getIntrinsicWidth() * ratio);
+			bgHeight = cropWidth
+					? bgHeight
+					: (int) (mBackground.getIntrinsicHeight() * ratio);
 		} else {
 			float ratio = (float) width / mBackground.getIntrinsicWidth();
 			bgHeight = (int) (mBackground.getIntrinsicHeight() * ratio);
@@ -152,9 +176,12 @@ public class ParallaxLayout extends FrameLayout implements TiltSensor.SensorCall
 		mBackgroundInitYPosition = -(bgHeight - height) / 2;
 	}
 
-	private float angleToTranslation(float degrees) {
+	private float angleToTranslation(float degrees, int direction) {
 		int bgWidth = mBackground.getBounds().right - mBackground.getBounds().left;
-		int diff = Math.abs((getRight() - getLeft()) - bgWidth);
+		int bgHeight = mBackground.getBounds().bottom - mBackground.getBounds().top;
+		int diff = direction == DIRECTION_HORIZONTAL
+				? Math.abs((getRight() - getLeft()) - bgWidth)
+				: Math.abs((getBottom() - getTop()) - bgHeight);
 		float translation = mParallaxSpeed * degrees;
 
 		if (translation > diff / 2) {
